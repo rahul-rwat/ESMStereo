0e7afe60e66abc41a2663d8820e60ed8b45d6bc1
m2219 <tahmasebi.2219@gmail.com>
2025-07-03 17:25:18 +0100

    cleand

--- kitti_publisher/build/kitti_publisher/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stereo/ESMStereo/kitti_publisher/src/kitti_publisher
-# Build directory: /home/stereo/ESMStereo/kitti_publisher/build/kitti_publisher
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
