bcf78032f52c8a90282e9d4bab2b3761f5e0668f
m2219 <tahmasebi.2219@gmail.com>
2025-07-16 15:25:03 +0100

    cleaned, types handled, duplicates removed

--- kitti_publisher_ess/launch/kitti_publisher_ess_cuda_node.launch.py
+++ kitti_publisher_ess/launch/kitti_publisher_ess_cuda_node.launch.py
@@ -10,9 +10,17 @@ def generate_launch_description():
         executable='kitti_publisher_ess_cuda_node',
         name='kitti_publisher_ess_cuda_node',
         output='screen',
-        parameters=[{'kitti_path': './../vkitti/vkitti/Scene01/fog/frames/rgb/',
-                     'depth_kitti_path': './../vkitti/depth/Scene01/fog/frames/depth/',
-                     'plugin_path': './../../dnn_stereo_disparity/plugins/x86_64/ess_plugins.so'}]
+        parameters=[
+            {'kitti_path': './../../vkitti/rgb/Scene02/rain/frames/rgb/'},
+            {'plugin_path': './../../dnn_stereo_disparity/plugins/x86_64/ess_plugins.so'},
+            {'depth_kitti_path': './../../vkitti/depth/Scene02/rain/frames/depth/'},
+            {'model_path': '/tmp/ess.plan'},
+            {'record_video': False},
+            {'net_input_width': 960},
+            {'net_input_height': 576},
+            {'fx': 725.0087},
+            {'baseline': 0.532725},
+            {'max_disp': 192.0}]
     )
 
     return LaunchDescription([
--- kitti_publisher_ess/src/kitti_publisher_ess_cuda_node.cpp
+++ kitti_publisher_ess/src/kitti_publisher_ess_cuda_node.cpp
@@ -1,65 +1,44 @@
+// cpp
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <dlfcn.h>
+
+// ROS
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <signal.h>
 #include "geometry_msgs/msg/transform_stamped.hpp"
-
-#include <NvInfer.h>
-#include <NvInferRuntime.h>
-#include <iostream>
-#include <fstream>
-#include <opencv2/opencv.hpp>
-#include <cuda_runtime_api.h>
-#include <vector>
-#include <string>
-#include <filesystem>
-#include <chrono>
-#include <opencv2/videoio.hpp>
-#include <opencv2/calib3d.hpp>
-#include <cv_bridge/cv_bridge.h>
 #include "tf2_ros/transform_broadcaster.h"
-
-#include <opencv2/ximgproc/edge_filter.hpp>
-
-#include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <cv_bridge/cv_bridge.h>
+
+// opencv
 #include <opencv2/opencv.hpp>
-#include <filesystem>
-#include <string>
-#include <vector>
-#include <dlfcn.h>
+#include <opencv2/videoio.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/ximgproc/edge_filter.hpp>
+#include <opencv2/opencv.hpp>
 
-namespace fs = std::filesystem;
-
-std::mutex queue_mutex;
-std::condition_variable queue_cv;
-bool shutdown_flag = false;
+// tensorrt
+#include <NvInfer.h>
+#include <NvInferRuntime.h>
+#include <cuda_runtime_api.h>
+#include <cv_bridge/cv_bridge.h>
 
 namespace fs = std::filesystem;
 using namespace std::chrono;
 
-int net_input_height_ = 576;
-int net_input_width_ = 960;
-constexpr float DEPTH_SCALE = 0.01f;
-constexpr float INVALID_DISP = 0.0f;
-int pad_right;
-int pad_bottom;
-double max_disp = 192;
-cv::Mat disp_filtered;
-float alpha = 0.4;
-double total_epe = 0.0;
-int frame_count = 0;
-bool record_video = true;  // Set to false to disable recording
-cv::VideoWriter video_writer;
-
-std::string model_path_ = "/tmp/ess.plan";
-
 nvinfer1::ICudaEngine* engine_{nullptr};
 nvinfer1::IExecutionContext* context_{nullptr};
-void* buffers_[4]{nullptr, nullptr, nullptr, nullptr};
 cudaStream_t stream_;
-int leftIndex_, rightIndex_, outputIndex_, outputConfidenceIndex_;
-size_t inputSize_, outputSize_, confidenceSize_;
+void* buffers_[4]{nullptr, nullptr, nullptr, nullptr};
+
+constexpr float DEPTH_SCALE = 0.01f;
+constexpr float INVALID_DISP = 0.0f;
 
 class Logger : public nvinfer1::ILogger
 {
@@ -157,6 +136,9 @@ double computeD1(const cv::Mat& disp_gt,
     return valid_px ? 100.0 * bad_px / valid_px : 0.0;
 }
 
+
+double total_epe = 0.0;
+int frame_count = 0;
 void visualize_and_record_disparity(
     const cv::Mat& disparity,
     const cv::Mat& disp_filtered_16,
@@ -165,15 +147,12 @@ void visualize_and_record_disparity(
     const cv::Mat& valid_mask,
     bool record_video,
     double elapsed_ms,
+    double fx,
+    double baseline,
+    int target_height,
+    int target_width,
     cv::VideoWriter& video_writer
 ) {
-
-    double fx = 725.0087;
-    double baseline = 0.532725;
-
-    // Target size
-    int target_height = 576;
-    int target_width = 960;
 
     cv::Mat disp_gt = depthToDisparity(depth_16, fx, baseline);
     cv::resize(disp_gt, disp_gt, cv::Size(target_width, target_height), 0, 0, cv::INTER_LINEAR);
@@ -202,13 +181,11 @@ void visualize_and_record_disparity(
 
     double max_val, min_val;
     cv::minMaxLoc(disp_filtered_16, &min_val, &max_val, nullptr, nullptr, valid_mask);
-    std::cout << "Disparity range: [" << min_val << ", " << max_val << "]" << std::endl;
     cv::Mat disp_norm, disp_color;
 
     disp_filtered_16.convertTo(disp_norm, CV_8UC1, -255.0 / (max_val - min_val), 255.0 * max_val / (max_val - min_val));
     cv::applyColorMap(disp_norm, disp_color, cv::COLORMAP_MAGMA);
 
-    // Convert grayscale left image to BGR if needed
     cv::Mat left_color;
     if (left_img.channels() == 1) {
         cv::cvtColor(left_img, left_color, cv::COLOR_GRAY2BGR);
@@ -216,16 +193,13 @@ void visualize_and_record_disparity(
         left_color = left_img.clone();
     }
 
-    // Match dimensions if needed
     if (left_color.size() != disp_color.size()) {
         cv::resize(left_color, left_color, disp_color.size());
     }
 
-    // Concatenate images horizontally
     cv::circle(disp_color, cv::Point(center_x, center_y), 5, cv::Scalar(255, 0, 0), -1);
     cv::putText(disp_color, depth_text, cv::Point(center_x + 10, center_y - 10), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 0, 0), 2);
 
-    // Elapsed time annotation (FPS)
     std::ostringstream oss, oss_epe;
     oss << std::fixed << std::setprecision(2) << 1000.0 / elapsed_ms << " HZ";
     std::string text = oss.str();
@@ -246,16 +220,10 @@ void visualize_and_record_disparity(
 
     cv::Mat combined;
     cv::vconcat(left_color, disp_color, combined);
-
-    // Output image
     cv::resize(combined, combined, cv::Size(target_width, target_height), 0, 0, cv::INTER_LINEAR);
-
-
-    // Show in window
     cv::imshow("Left + Disparity", combined);
     cv::waitKey(1);
 
-    // Write to video file
     if (record_video && !video_writer.isOpened()) {
         std::string output_path = "disparity_output.mp4";
         int fourcc = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
@@ -270,41 +238,30 @@ void visualize_and_record_disparity(
 }
 
 
-float* preprocess_image(const cv::Mat& img, const int net_input_width, const int net_input_height) {
+float* preprocess_image(const cv::Mat& img, const int target_width, const int target_height) {
 
     int w = img.cols;
     int h = img.rows;
     int m = 32;
 
-    // Target size
-    int target_height = 576;
-    int target_width = 960;
-
-    // Output image
     cv::Mat img_rgb;
     cv::resize(img, img_rgb, cv::Size(target_width, target_height), 0, 0, cv::INTER_LINEAR);
 
-    // Convert to float and normalize to [0, 1]
     img_rgb.convertTo(img_rgb, CV_32FC3, 1.0 / 255.0);
 
-    // Split channels
     std::vector<cv::Mat> channels(3);
     cv::split(img_rgb, channels);
 
-    // Mean and std (same as PyTorch)
     float mean_vals[3] = {0.0f, 0.0f, 0.0f};
     float std_vals[3]  = {1.0f, 1.0f, 1.0f};
 
-    // Normalize each channel
     for (int c = 0; c < 3; ++c) {
         channels[c] = (channels[c] - mean_vals[c]) / std_vals[c];
     }
 
-    // Allocate CHW float buffer
     int size = 3 * img_rgb.rows * img_rgb.cols;
     float* chw = new float[size];
 
-    // Fill in CHW order
     int idx = 0;
     for (int c = 0; c < 3; ++c) {
         for (int h = 0; h < img_rgb.rows; ++h) {
@@ -351,20 +308,20 @@ nvinfer1::ICudaEngine* loadEngine(const std::string& engineFile) {
     return engine;
 }
 
-bool initializeTensorRT(const std::string plugin_path) {
+bool initializeTensorRT(const std::string model_path, const std::string plugin_path, int net_input_height_, int net_input_width_,
+                        int& leftIndex_, int& rightIndex_, int& outputIndex_, int& confidenceIndex_,
+                        size_t& inputSize_, size_t& outputSize_, size_t& confidenceSize_) {
 
     loadPluginLibrary(plugin_path);
-    engine_ = loadEngine(model_path_);
+    engine_ = loadEngine(model_path);
     if (!engine_) {
          std::cerr << "Error loading engine" << std::endl;
     }
 
     context_ = engine_->createExecutionContext();
 
-    // Set up stream
     cudaStreamCreate(&stream_);
 
-    // Input/output dims
     inputSize_ = 1 * 3 * net_input_height_ * net_input_width_ * sizeof(float);
     outputSize_ = 1 * net_input_height_ * net_input_width_ * sizeof(float);
     confidenceSize_ = 1 * net_input_height_ * net_input_width_ * sizeof(float);
@@ -377,9 +334,8 @@ bool initializeTensorRT(const std::string plugin_path) {
     leftIndex_ = -1;
     rightIndex_ = -1;
     outputIndex_ = -1;
-    outputConfidenceIndex_ = -1;
+    confidenceIndex_ = -1;
 
-    // Find tensor indices
     for (int i = 0; i < engine_->getNbIOTensors(); ++i) {
         const char* name = engine_->getIOTensorName(i);
 
@@ -393,20 +349,17 @@ bool initializeTensorRT(const std::string plugin_path) {
             if (strcmp(name, outputName.c_str()) == 0) outputIndex_ = i;
 
         for (const auto& confidenceName : confidenceNames)
-            if (strcmp(name, confidenceName.c_str()) == 0) outputConfidenceIndex_ = i;
+            if (strcmp(name, confidenceName.c_str()) == 0) confidenceIndex_ = i;
     }
 
-    // Set shapes
     nvinfer1::Dims4 inputDims = {1, 3, net_input_height_, net_input_width_};
     context_->setInputShape(engine_->getIOTensorName(leftIndex_), inputDims);
     context_->setInputShape(engine_->getIOTensorName(rightIndex_), inputDims);
 
-    // Set tensor addresses
-    // Allocate buffers
     cudaMalloc(&buffers_[leftIndex_], inputSize_);
     cudaMalloc(&buffers_[rightIndex_], inputSize_);
     cudaMalloc(&buffers_[outputIndex_], outputSize_);
-    cudaMalloc(&buffers_[outputConfidenceIndex_], confidenceSize_);
+    cudaMalloc(&buffers_[confidenceIndex_], confidenceSize_);
 
     return true;
 }
@@ -414,19 +367,23 @@ bool initializeTensorRT(const std::string plugin_path) {
 
 class KittiImagePublisher : public rclcpp::Node {
 public:
-    KittiImagePublisher() : Node("kitti_image_publisher"), current_index_(0) {
+    KittiImagePublisher() : Node("kitti_image_publisher"), current_index(0) {
         RCLCPP_INFO(this->get_logger(), "Image Publisher Node Started!");
 
         // Parameters
-        this->declare_parameter<std::string>("kitti_path", "./vkitti");
-        this->declare_parameter<std::string>("depth_kitti_path", "./depth");
-        this->declare_parameter<std::string>("plugin_path", "./plugin.so");
 
-        kitti_path = this->get_parameter("kitti_path").as_string();
-        depth_kitti_path = this->get_parameter("depth_kitti_path").as_string();
-        plugin_path = this->get_parameter("plugin_path").as_string();
+        kitti_path = this->declare_parameter<std::string>("kitti_path", "./vkitti");
+        depth_kitti_path = this->declare_parameter<std::string>("depth_kitti_path", "./vkitti/depth");
+        model_path = this->declare_parameter<std::string>("model_path", "/tmp/StereoModel_576_960_fp16.plan");
+        plugin_path = this->declare_parameter<std::string>("plugin_path", "./plugin.so");
+        record_video = this->declare_parameter<bool>("record_video", true);
+        net_input_width_ = this->declare_parameter<int>("net_input_width", 576);
+        net_input_height_ = this->declare_parameter<int>("net_input_height", 960);
+        fx = this->declare_parameter<double>("fx", 707.0912);
+        baseline = this->declare_parameter<double>("baseline", 0.536);
+        max_disp = this->declare_parameter<double>("max_disp", 192);
 
-        fps_ = 150;
+        fps = 150;
         left_dir_ = kitti_path + "/Camera_0/";
         right_dir_ = kitti_path + "/Camera_1/";
         depth_dir_ = depth_kitti_path + "/Camera_0/";
@@ -458,12 +415,13 @@ public:
         right_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/cam1/image_raw", 10);
         disparity_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/disparity/image_raw", 10);
 
-        timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(1000 / fps_),
+        timer = this->create_wall_timer(
+            std::chrono::milliseconds(1000 / fps),
             std::bind(&KittiImagePublisher::publishImages, this)
         );
 
-        if (!initializeTensorRT(plugin_path)) {
+        if (!initializeTensorRT(model_path, plugin_path, net_input_height_, net_input_width_,
+                                leftIndex_, rightIndex_, outputIndex_, confidenceIndex_, inputSize_, outputSize_, confidenceSize_)) {
             RCLCPP_ERROR(this->get_logger(), "TensorRT initialization failed!");
             rclcpp::shutdown();
         }
@@ -472,20 +430,20 @@ public:
 private:
     void publishImages() {
 
-        if (current_index_ >= left_images_.size()) {
-            current_index_ = 0;
+        if (current_index >= left_images_.size()) {
+            current_index = 0;
             exit(0);
         }
 
-        cv::Mat left_img = cv::imread(left_images_[current_index_], cv::IMREAD_COLOR);
-        cv::Mat right_img = cv::imread(right_images_[current_index_], cv::IMREAD_COLOR);
-        cv::Mat depth_16 = cv::imread(depth_images_[current_index_], cv::IMREAD_ANYCOLOR | cv::IMREAD_ANYDEPTH);
+        left_img = cv::imread(left_images_[current_index], cv::IMREAD_COLOR);
+        right_img = cv::imread(right_images_[current_index], cv::IMREAD_COLOR);
+        depth_16 = cv::imread(depth_images_[current_index], cv::IMREAD_ANYCOLOR | cv::IMREAD_ANYDEPTH);
 
         int original_height = left_img.rows;
         int original_width = left_img.cols;
 
         if (left_img.empty() || right_img.empty()) {
-            RCLCPP_WARN(this->get_logger(), "Failed to read images at index %d", current_index_);
+            RCLCPP_WARN(this->get_logger(), "Failed to read images at index %d", current_index);
             return;
         }
 
@@ -504,7 +462,6 @@ private:
         left_pub_->publish(*left_msg);
         right_pub_->publish(*right_msg);
 
-        // Run stereo inference
         float* outputData = new float[1 * net_input_height_ * net_input_width_];
         float* confidenceOutputData = new float[1 * net_input_height_ * net_input_width_];
         float* inputLeft = preprocess_image(left_img, net_input_width_, net_input_height_);
@@ -512,14 +469,13 @@ private:
 
         auto start = high_resolution_clock::now();
 
-        // Copy input data to device
         cudaMemcpyAsync(buffers_[leftIndex_], inputLeft, inputSize_, cudaMemcpyHostToDevice, stream_);
         cudaMemcpyAsync(buffers_[rightIndex_], inputRight, inputSize_, cudaMemcpyHostToDevice, stream_);
 
         context_->setTensorAddress(engine_->getIOTensorName(leftIndex_), buffers_[leftIndex_]);
         context_->setTensorAddress(engine_->getIOTensorName(rightIndex_), buffers_[rightIndex_]);
         context_->setTensorAddress(engine_->getIOTensorName(outputIndex_), buffers_[outputIndex_]);
-        context_->setTensorAddress(engine_->getIOTensorName(outputConfidenceIndex_), buffers_[outputConfidenceIndex_]);
+        context_->setTensorAddress(engine_->getIOTensorName(confidenceIndex_), buffers_[confidenceIndex_]);
 
         // Run inference
         if (!context_->enqueueV3(stream_)) {
@@ -532,14 +488,11 @@ private:
         double elapsed_ms = duration<double, std::milli>(end - start).count();
         std::cout << "Elapsed time =: " << elapsed_ms << " ms" << std::endl;
 
-        // Copy output back to host
-        cudaMemcpyAsync(confidenceOutputData, buffers_[outputConfidenceIndex_], outputSize_, cudaMemcpyDeviceToHost, stream_);
+        cudaMemcpyAsync(confidenceOutputData, buffers_[confidenceIndex_], outputSize_, cudaMemcpyDeviceToHost, stream_);
         cudaMemcpyAsync(outputData, buffers_[outputIndex_], outputSize_, cudaMemcpyDeviceToHost, stream_);
 
-        // Convert and display
         cv::Mat disp_mat(net_input_height_, net_input_width_, CV_32FC1, outputData);
 
-        // Crop the disparity cv::Mat to remove padding
         if (pad_bottom > 0 || pad_right > 0) {
             disp_mat = disp_mat(cv::Rect(0, 0, original_width, original_height));
         }
@@ -548,6 +501,7 @@ private:
         cv::medianBlur(disp_mat, disp_filtered, 5);
 
         // 2. Temporal smoothing
+        //float alpha = 0.4;
         //static cv::Mat prev_disp;
         //if (prev_disp.empty()) prev_disp = disp_filtered.clone();
         //cv::addWeighted(disp_filtered, alpha, prev_disp, 1.0 - alpha, 0, disp_filtered);
@@ -567,6 +521,10 @@ private:
             valid_mask,
             record_video,
             elapsed_ms,
+            fx,
+            baseline,
+            net_input_height_,
+            net_input_width_,
             video_writer
         );
 
@@ -585,28 +543,22 @@ private:
         disparity_pub_->publish(*disp_msg);
 
 
-        current_index_++;
+        current_index++;
     }
 
+    cv::Mat valid_mask, left_img, right_img, depth_16;
+    std::string kitti_path, model_path, plugin_path, depth_kitti_path, left_dir_, right_dir_, depth_dir_;
+    std::vector<std::string> left_images_, right_images_, depth_images_;
+    cv::Mat disp_filtered, disp_filtered_16;
+    double fx, baseline, max_disp;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr left_pub_, right_pub_, disparity_pub_;
+    rclcpp::TimerBase::SharedPtr timer;
+    bool record_video;
+    int net_input_width_, net_input_height_, current_index, fps, pad_right, pad_bottom;
+    int leftIndex_, rightIndex_, outputIndex_, confidenceIndex_;
+    cv::VideoWriter video_writer;
+    size_t inputSize_,  outputSize_, confidenceSize_;
 
-    std::string kitti_path;
-    std::string depth_kitti_path;
-    std::string plugin_path;
-    std::string left_dir_;
-    std::string right_dir_;
-    std::string depth_dir_;
-    std::vector<std::string> left_images_;
-    std::vector<std::string> right_images_;
-    std::vector<std::string> depth_images_;
-    cv::Mat disp_filtered;
-    cv::Mat disp_filtered_16;
-    size_t current_index_;
-    int fps_;
-
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr left_pub_;
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr right_pub_;
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr disparity_pub_;
-    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char* argv[]) {
